6062e7e703cac39f9e81024bacc9ac215c01d31d
tediscript <tediscript@gmail.com>
2013-03-06 14:30:59 +0700

    Initial Commit

---
+++ CustomAlert/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomAlert
+//
+//  Created by Tedi Fibri on 3/6/13.
+//  Copyright (c) 2013 tediscript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
